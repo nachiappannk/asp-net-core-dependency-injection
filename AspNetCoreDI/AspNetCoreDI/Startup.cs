fc3c32bab28673e16b78426827b485fcddfc4162
Nachiappan <nachiapan@gmail.com>
2022-01-06 20:50:17 +0530

    Fixed dependency injection

--- AspNetCoreDI/AspNetCoreDI/Startup.cs
+++ AspNetCoreDI/AspNetCoreDI/Startup.cs
@@ -1,3 +1,4 @@
+using AspNetCoreDI.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,9 @@ namespace AspNetCoreDI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetCoreDI", Version = "v1" });
             });
+
+            services.AddSingleton<Dependency>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
